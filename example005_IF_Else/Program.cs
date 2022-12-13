5ee9386c9b1830ab0a35c7684cffa8057c6215dd
Evgeny.A <akimoveo@gmail.com>
2022-12-14 00:54:53 +0400

    ветвление

---
+++ example005_IF_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("назовите себя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("привет МАША!!!");
+}
+else
+{
+Console.WriteLine("привет");
+Console.Write(username);
+}
