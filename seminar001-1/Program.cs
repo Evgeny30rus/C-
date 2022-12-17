167ae2712cf4bd247972a8c3c21ac73b6e56ad37
Evgeny.A <akimoveo@gmail.com>
2022-12-17 11:56:31 +0400

    seminar1-1

--- seminar001-1/Program.cs
+++ seminar001-1/Program.cs
@@ -1,21 +1,16 @@
-﻿Console.WriteLine("введите число а");
+﻿
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
 
-// string a = Console.ReadLine();
-// double b = Convert.ToInt32(Console.ReadLine())
-
-// Console.WriteLine(a);
-
-int a = Convert.ToInt32(Console.ReadLine()); 
-
-Console.WriteLine("введите число b");
-int b = Convert.ToInt32(Console.ReadLine());
-
-
-if (a == b*b)  // != не равенство
-{
-    Console.WriteLine("да");
+while (a < 1 || a > 7){
+	Console.WriteLine("Вы ошиблись!! Введите число: ");
+	a = Convert.ToInt32(Console.ReadLine());
 }
-else
-{
-   Console.WriteLine("нет");
-}
+
+if (a==1) Console.WriteLine("Понедельник");
+else if (a==2) Console.WriteLine("Вторник");
+else if (a==3) Console.WriteLine("Среда");
+else if (a==4) Console.WriteLine("Четверг");
+else if (a==5) Console.WriteLine("Пятница");
+else if (a==6) Console.WriteLine("Суббота");
+else if (a==7) Console.WriteLine("Воскресенье");
