e4d8e391f2fbfdb9e0300bced694a0a1b5bb6d5b
Evgeny.A <akimoveo@gmail.com>
2022-12-17 11:50:49 +0400

    seminar1-1

---
+++ seminar001-1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите число а");
+
+// string a = Console.ReadLine();
+// double b = Convert.ToInt32(Console.ReadLine())
+
+// Console.WriteLine(a);
+
+int a = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a == b*b)  // != не равенство
+{
+    Console.WriteLine("да");
+}
+else
+{
+   Console.WriteLine("нет");
+}
